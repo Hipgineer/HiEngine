1ac83996feacb456bb42f228f840fd887c38b1b0
JsToys <94050820+Hipgineer@users.noreply.github.com>
2022-06-16 22:14:23 +0900

    create box with min/max points 2
    create box with min/max points2

--- src/scenes/boxDrop.h
+++ src/scenes/boxDrop.h
@@ -1,3 +1,5 @@
+// Test Scene
+// 
 class BoxDrop : public Scene
 {
 public :
@@ -11,8 +13,9 @@ public :
         // TODO: Voxelize 3d object to make particles
         // ex) 
         // int64_t const initParticleNumber = voxelizer->CreateParticles(&g_buffer->m_positions);
-
-        g_buffer->m_radius = 0.1;      
+ 
+        g_buffer->m_commonParam.radius  = 0.01;      
+        g_buffer->m_commonParam.dt      = 0.001;      
 
         // Box Generate
         int32_t xNum = 10;
@@ -24,10 +27,10 @@ public :
 
         for (int32_t ii = 0 ; ii < xNum ; ++ii)
             for (int32_t jj = 0 ; jj < yNum ; ++jj)
-                for (int32_t kk = 0 ; kk < yNum ; ++kk)
+                for (int32_t kk = 0 ; kk < zNum ; ++kk)
                 {
-                    g_buffer->m_positions.push_back(glm::vec3(ii*g_buffer->m_radius, jj*g_buffer->m_radius, kk*g_buffer->m_radius));
-                    g_buffer->m_velocities.push_back(glm::vec3(0.0, 0.0, 0.0));
+                    g_buffer->m_positions.push_back(glm::vec3(ii*g_buffer->m_commonParam.radius, jj*g_buffer->m_commonParam.radius, kk*g_buffer->m_commonParam.radius));
+                    g_buffer->m_velocities.push_back(glm::vec3(0.01, 0.0, 0.0));
                 }
     }
 };
