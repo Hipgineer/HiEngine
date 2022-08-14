15e9515121fbb1f38d86175ae92ac5bc6ab27e83
JsToys <94050820+Hipgineer@users.noreply.github.com>
2022-08-14 14:25:27 +0900

    add outline render for particles
    add outline render for particles

--- shader/simplePoint.fs
+++ shader/simplePoint.fs
@@ -35,36 +35,41 @@ void main() {
     float z = sqrt(1.0 - (pow(x, 2.0) + pow(y, 2.0)));
     vec4 pixelNorm = vec4(x, y, z, 0.0); // positions of fragment
     float mag = dot(pixelNorm.xy, pixelNorm.xy);
-    if(mag > 1.0) discard;
 
-    // pixelNorm = transpose(pointTransform)*normalize(pixelNorm);
-    pixelNorm = normalize(pixelNorm);
+    if(mag > 1.0) 
+        discard;
+    else if ( mag > 0.8)
+        fragColor = vec4(vec3(0.0,0.0,0.0), 1.0);
+    else {
+        // pixelNorm = transpose(pointTransform)*normalize(pixelNorm);
+        pixelNorm = normalize(pixelNorm);
 
-    // Light Interactions
-    vec3 ambient = material.diffuse * light.ambient;
-    vec3 result = vrtColor;
-    
-    // Intensity according to distance to object from light.
-    float dist = length(light.position - vrtPos);
-    vec3 distPoly = vec3(1.0, dist, dist*dist);
-    float attenuation = 1.0 / dot(distPoly, light.attenuation);
+        // Light Interactions
+        vec3 ambient = material.diffuse * light.ambient;
+        vec3 result = vrtColor;
+        
+        // Intensity according to distance to object from light.
+        float dist = length(light.position - vrtPos);
+        vec3 distPoly = vec3(1.0, dist, dist*dist);
+        float attenuation = 1.0 / dot(distPoly, light.attenuation);
 
-    vec3 lightDir = (light.position - vrtPos) / dist;
-    float theta = dot(lightDir, normalize(-light.direction));
-    float intensity = clamp((theta - light.cutoff[1]) / (light.cutoff[0] - light.cutoff[1]), 0.0, 1.0);
+        vec3 lightDir = (light.position - vrtPos) / dist;
+        float theta = dot(lightDir, normalize(-light.direction));
+        float intensity = clamp((theta - light.cutoff[1]) / (light.cutoff[0] - light.cutoff[1]), 0.0, 1.0);
 
-    if (intensity > 0.0 ) {
-        lightDir = normalize((transpose(inverse(viewTransform)) * vec4(lightDir, 0.0)).xyz);
-        float   diff = max(dot(pixelNorm.xyz, lightDir), 0.0);
-        vec3    diffuse = diff * material.diffuse * light.diffuse;
-        vec3    viewDir = (transpose(inverse(viewTransform)) * vec4(normalize(cameraPos - vrtPos),0.0)).xyz;
-        vec3    reflectDir = reflect(-lightDir, pixelNorm.xyz);
-        float   spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-        vec3    specular = spec * material.specular * light.specular;
-        result += (diffuse + specular) * intensity;
+        if (intensity > 0.0 ) {
+            lightDir = normalize((transpose(inverse(viewTransform)) * vec4(lightDir, 0.0)).xyz);
+            float   diff = max(dot(pixelNorm.xyz, lightDir), 0.0);
+            vec3    diffuse = diff * material.diffuse * light.diffuse;
+            vec3    viewDir = (transpose(inverse(viewTransform)) * vec4(normalize(cameraPos - vrtPos),0.0)).xyz;
+            vec3    reflectDir = reflect(-lightDir, pixelNorm.xyz);
+            float   spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+            vec3    specular = spec * material.specular * light.specular;
+            result += (diffuse + specular) * intensity;
+        }
+
+        result *= attenuation;
+
+        fragColor = vec4(result, 1.0);
     }
-
-    result *= attenuation;
-
-    fragColor = vec4(result, 1.0);
 }
